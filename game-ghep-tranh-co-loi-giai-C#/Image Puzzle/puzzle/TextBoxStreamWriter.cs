32eb07b5f5757eff623023a212fb5dfae4a1a21c
huedo2102 <huueed@gmail.com>
2024-03-06 13:17:24 +0700

    new

---
+++ game-ghep-tranh-co-loi-giai-C#/Image Puzzle/puzzle/TextBoxStreamWriter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using System.IO;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public class TextBoxStreamWriter : TextWriter
+    {
+        TextBox _output = null;
+
+        public TextBoxStreamWriter(TextBox output)
+        {
+            _output = output;
+        }
+
+        public override void Write(char value)
+        {
+            base.Write(value);
+            _output.AppendText(value.ToString());
+
+        }
+
+        public override Encoding Encoding
+        {
+            get { return System.Text.Encoding.UTF8; }
+        }
+    }
+}
